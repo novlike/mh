0bf08b6cdf063db049770fd11b378dd5e8303bc3
novlike <https://github.com/novlike>
2018-11-12 10:52:30 +0100

    add vendor

---
+++ src/Controller/TestController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hlecouey
+ * Date: 12/11/2018
+ * Time: 10:04
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class TestController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+
+    }
+}
